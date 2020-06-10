7eec50799e138f2d28d804ca68903f9ba0ec6c1b
Stacey Louis <stacey-louis@lambdastudents.com>
2020-06-10 19:41:47 -0400

    updated

--- index.js
+++ index.js
@@ -40,11 +40,17 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
+function is31Flavors(arr){
 
-    /*code here*/
+   if(arr.length === 31){
+       return true;
+   } else {
+       return false;
+   }
 
 }
+
+console.log(is31Flavors(originalFlavors));
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -57,13 +63,12 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
-
-    /*code here*/
-
+function addFlavor(arr, flav){
+arr.unshift(flav)
+console.log(arr)
 }
 
-
+console.log(addFlavor(originalFlavors));
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
 Your function should accept:
@@ -74,11 +79,12 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
+function removeLastFlavor(arr, flav){
+arr.pop(flav)
+console/log(arr)
 
 }
+console.log(originalFlavors());
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -89,9 +95,10 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
+function getFlavorByIndex(arr, pos){
 
-    /*code here*/
+    
+
 
 }
 
@@ -108,9 +115,9 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
+function removeFlavorByName(arr, flav){
 
-    /*code here*/
+
 
 }
 
@@ -144,9 +151,9 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
+function filterByWord(arr, str){
+ let filtArr = [];
+ for (let i = )
 
 }
 
